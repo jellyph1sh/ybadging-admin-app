9a3cb6da08a58c4c4120729fa59bb42e6d416664
matiasbellaud <bellaud.matias@gmail.com>
2024-04-22 13:39:09 +0200

    :building_construction: | change application structure

---
+++ Ybadging-admin-app/src/app.jsx
@@ -0,0 +1,13 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(<h2>Hello from React in Electron!</h2>, document.body);
+
+import React from "react";
+import ReactDOM from "react-dom";
+import Route from "./route.jsx";
+
+ReactDOM.render(<React.StrictMode><Route /></React.StrictMode>, document.body);
+// root.render(
+//   <React.StrictMode><Route /></React.StrictMode>
+// );
