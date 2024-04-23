ddd9ade16793854192aca25f402f68a82de05500
matiasbellaud <bellaud.matias@gmail.com>
2024-04-23 10:54:50 +0200

    :construction: | start create student

--- Ybadging-admin-app/src/pages/adminPage.jsx
+++ Ybadging-admin-app/src/pages/adminPage.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Lessons from "../components/lessons.jsx";
 import CreateLessons from "../components/createLesson.jsx";
+import CreateStudent from "../components/createStudent.jsx";
 
 const AdminPage = () => {
 
   return (
     <body >
-      <CreateLessons/>
+      <CreateStudent/>
     </body>
   );
 };
